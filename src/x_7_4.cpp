9cde8c959e0f9575b9b00610607e186c7fad3a40
Thomas HOULLIER <pro@houllier.net>
2024-05-20 21:52:36 +0200

    x_7_4 done

---
+++ src/x_7_4.cpp
@@ -0,0 +1,31 @@
+// Find 10 different examples of nonportable C++ code.
+
+// 1. A library for STM32 microcontrollers:
+// https://github.com/andysworkshop/stm32plus
+
+// 2. Windows desktop programs:
+// https://learn.microsoft.com/en-us/cpp/windows/walkthrough-creating-windows-desktop-applications-cpp?view=msvc-170
+
+// 3. The CUDA C++ language
+// https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html
+
+// 4. Libraries for microsoft peripheral inputs
+// https://learn.microsoft.com/en-us/windows/mixed-reality/develop/native/keyboard-mouse-and-controller-input-cpp
+
+// 5. The C POSIX library
+// https://en.wikipedia.org/wiki/C_POSIX_library
+
+// 6. DirectX API
+// https://learn.microsoft.com/en-us/windows/win32/direct3dgetstarted/building-your-first-directx-app
+
+// 7. SIMD extension instructions
+// https://en.wikipedia.org/wiki/Streaming_SIMD_Extensions
+
+// 8. Windows-specific file paths
+// "C:\\User\\..."
+
+// 9. Extended registers
+// eg. ZMM31
+
+// 10. Relying on the size in bits of any type beyond the required minima
+//     specified by the standard: eg. a char assumed to be always 8 bits long.
